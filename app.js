7e13a56baec2391bb04240ce7fa0bbd3f486e0fe
rayuniday <rayuniday@naver.com>
2022-02-23 03:43:09 +0900

    #2.8 Functions part Two

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-// console.log("Hello my name is Nico");
-// console.log("Hello my name is Dal");
-// console.log("Hello my name is Shigatsu");
-// console.log("Hello my name is Flynn");
-
-function sayHello() {
-  console.log("Hello my name is C");
+function sayHello(nameOfPerson, age) {
+  console.log("Hello my name is " + nameOfPerson + " and I'm " + age);
 }
 
-sayHello("nico");
-sayHello("dal");
-sayHello("lynn");
+sayHello("nico", 10);
+sayHello("dal", 23);
+sayHello("lynn", 21);
+
+function plus(firstNumber, secondNumber) {
+  console.log(firstNumber + secondNumber);
+}
+function divide(a, b) {
+  console.log(a / b);
+}
+
+plus(60, 8);
+divide(98, 20);
+
+const player = {
+  name: "nico",
+  sayHello: function (otherPersonsName) {
+    console.log("hello " + otherPersonsName + " nice to meet you!");
+  },
+};
+console.log(player.name);
+player.sayHello("lynn");
+player.sayHello("nico");
